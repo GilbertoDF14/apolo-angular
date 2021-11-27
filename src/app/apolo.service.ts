6116e7b976c7f7306e73935f1a9ceadad6e0312a
GilbertoDF14 <48808299+GilbertoDF14@users.noreply.github.com>
2021-11-27 17:58:28 -0600

    TemasFaltaMios

--- src/app/apolo.service.ts
+++ src/app/apolo.service.ts
@@ -46,10 +46,36 @@ export class ApoloService {
     return this.http.get(URL + "cursos",{headers:headers});
   }
 
+  idcurso(){
+    let headers=new HttpHeaders;
+    headers=headers.append('Authorization',this.cuenta.token);
+    console.log(this.cuenta.token);
+    return this.http.get(URL + "idcurso",{headers:headers});
+  }
+
   miscursos(){
     let headers=new HttpHeaders;
     headers=headers.append('Authorization',this.cuenta.token);
     const us=this.cuenta.user;
     return this.http.get(URL + "mycourses/"+us,{headers:headers});
+  }
+
+  addCurso(profesor:string,nombre:string,descripcion:string){
+    let headers=new HttpHeaders;
+    let form = new FormData;
+    headers=headers.append('Authorization',this.cuenta.token);
+    form.append('profesor',profesor);
+    form.append('nombre',nombre);
+    form.append('descripcion',descripcion);
+    return this.http.post(URL + "curso",form,{headers:headers});
+  }
+
+  addMiCurso(curso:any,alumno:string){
+    let headers=new HttpHeaders;
+    let form = new FormData;
+    headers=headers.append('Authorization',this.cuenta.token);
+    form.append('curso',curso);
+    form.append('alumno',alumno);
+    return this.http.post(URL + "mycourse",form,{headers:headers});
   }
 }
--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -9,12 +9,18 @@ import { ToastrService } from 'ngx-toastr';
 export class InicioComponent implements OnInit {
 
   miscursos:any;
+  rol='';
+  nombre='';
+  descripcion='';
+  profesor='';
 
   constructor(private apolo:ApoloService, private msgbox:ToastrService) { }
 
   ngOnInit(): void {
     this.apolo.getCuenta();
     this.llenarTabla();
+    this.rol = this.apolo.getCuenta().rol;
+
   }
 
   llenarTabla(){
@@ -25,6 +31,39 @@ export class InicioComponent implements OnInit {
       },
       error=>{
         console.log(error);
+      }
+    )
+  }
+
+  agregar(){
+    this.profesor=this.apolo.getCuenta().user
+    this.apolo.addCurso(this.profesor,this.nombre,this.descripcion).subscribe(
+      datos => {
+        this.llenarmicurso();
+        console.log(datos);
+      }
+    );
+  }
+
+  idc:any
+  cr=0;
+  al='';
+  llenarmicurso(){
+    this.al=this.apolo.getCuenta().user
+    this.apolo.idcurso().subscribe(
+      datos=>{
+        this.idc=datos;
+        //Aqui sacar el id para enviarlo
+        this.cr=this.idc[0].id;
+        this.idc.forEach(element => {
+          'id'
+        });
+        //
+        this.apolo.addMiCurso(this.cr,this.al).subscribe(
+          datos => {
+            this.llenarTabla();
+          }
+        );
       }
     )
   }
